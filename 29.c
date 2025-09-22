7a00a7163bd049b659064cca2d2aa21ec60c249e
Kp-0705 <kartavya0705@gmail.com>
2025-09-22 10:51:32 +0530

    Update 13.c file

--- 29.c
+++ 29.c
@@ -4,13 +4,35 @@ Name : 29.c
 Author : Kartavyakumar Patel
 Description : 29. Write a program to remove the message queue.
 
-Date : th sept, 2025
+Date : 22th sept, 2025
 ========================================================================================================================================================================================
 */
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/msg.h>
+#include<sys/ipc.h>
+int main()
+{
+key_t k=(key_t)'K';
+int mid;
+if((mid=msgget(k,IPC_CREAT|0666))==-1)
+{
+  perror("msgget");
+  exit(1);
+}
+if(msgctl(mid,IPC_RMID,NULL)==-1)
+{
+perror("msgctl");
+        exit(1);
+}
+ printf("Mes_que removed.\n");
+}
 
 /*
  Output:
 
+dell@dell-Inspiron-3593:~/Desktop/MT2025059/Hands-on_List2$ ./a.out
+Mes_que removed.
 
 */
